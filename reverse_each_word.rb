409b8ee6c2f898db896ab5e09eace832efe023f0
Roland McRae <mcraeroland@gmail.com>
2020-04-19 02:55:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|x| x.reverse }
-  new_array.join(" ")
+  words = sentence.split(" ")
+  reversed = []
+  reversed = words.collect {|x| x.reverse }
+  reversed.join(" ")
 end
