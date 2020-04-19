96dbd6d432c60dfe5b57c2102dd6a94d4e4368d4
Roland McRae <mcraeroland@gmail.com>
2020-04-19 02:50:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = sentence.split
-  for (i = 0, i < array.length, i++)
+  for (i = 0, i < array.length, i++){
     array[i].reverse
-  end
+  }
   return array
 end
